a31c3bd614bc0806dc246d549cadb8103a4620f7
JollyFuzz <mazcoding@gmail.com>
2025-07-26 21:30:32 +0300

    Добавлена FSM в бота

--- src/app.py
+++ src/app.py
@@ -1,22 +1,49 @@
 import types
 
 import telebot
+from telebot.handler_backends import State, StatesGroup
+from telebot.types import Message
+from telebot.custom_filters import StateFilter
 
-from bot.handler import get_question
+
+from bot.handler import get_question, handle_answer
 from config import Config
 from logger import logger
 
 cfg = Config()
 
+#TODO: вынести состояния
+class UserStates(StatesGroup):
+    ANSWERING = State()  # Пользователь отвечает на вопрос
+    WAITING_FEEADBACK = State() # Пользоавтель ждет обратную связь
+
 logger.info("Инициализация бота")
 
 bot = telebot.TeleBot(cfg.TG_TOKEN)
 
+@bot.message_handler(state=UserStates.ANSWERING)
+def repeat_answer(message: Message):
+    logger.debug("Получен ответ от пользователя")
+    text_answer = handle_answer(message)
+
+    bot.send_message(message.chat.id, f"Вы ответили {text_answer}")
+
+    bot.delete_state(message.from_user.id, message.chat.id)
+
 
 @bot.message_handler(commands=["next"])
-def handle_next(message):  # TODO: переименовать
+def send_question(message):
+    logger.debug("Пользователь запросил вопрос")
     response = get_question()
-    bot.reply_to(message, response)
+
+    bot.set_state(message.from_user.id, UserStates.ANSWERING)
+    logger.debug(f"Состояние пользователя измененно на {bot.get_state(message.from_user.id, message.chat.id)}")
+
+    bot.send_message(message.chat.id, response)
+    logger.debug(f"Пользователю отправлен вопрос {response}")
+    
 
 
-bot.polling(none_stop=True, interval=0)  # обязательная для работы бота часть
+bot.add_custom_filter(StateFilter(bot))
+bot.polling(none_stop=True)
+
